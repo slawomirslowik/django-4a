3c9e813cc7ea4b7b28e7aa55101b8f72bb2943d1
Slawomir Slowik <SlawomirSlowik@yahoo.pl>
2024-03-01 08:31:32 +0100

    updated model

--- myapp/polls/urls.py
+++ myapp/polls/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("counter/", views.counter, name="counter"),
-    path("basic/", views.basic, name="basic")
+    path("counter/", views.counter, name="counter")
 ]
