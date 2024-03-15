5afc849795faf46afef1c9081823cbd3e3f3367f
Slawomir Slowik <SlawomirSlowik@yahoo.pl>
2024-03-15 07:51:29 +0100

    deleted redundand views

--- myapp/polls/views.py
+++ myapp/polls/views.py
@@ -12,15 +12,3 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-
-def counter(request):
-    text_file = open("./templates/counter.html", "r")
-    page = text_file.read()
-    text_file.close()
-    return HttpResponse(page)
-
-def basic(request):
-    text_file = open("./templates/basic.html", "r")
-    page = text_file.read()
-    text_file.close()
-    return HttpResponse(page)
